fe2f01d2336cb30a437bd2b603861f991425cfe8
Carlos Leite <126902044+carlosebl@users.noreply.github.com>
2024-01-05 15:48:50 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+const multiplica = (num1: number, num2: number): number => num1 * num2;
+
+console.log(multiplica(2, 3));
+
+const saudacao = (nome: string): string => `Olá, ${nome}!`;
+
+console.log(saudacao('Carlos'));
